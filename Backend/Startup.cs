750379eeec793b40643690b314734e95304be118
Stenger, Michael <michael.stenger@sivantos.com>
2021-09-02 10:12:37 +0200

    switching to azure signalr

--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -11,7 +11,10 @@ namespace Backend
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSignalR();
+            services
+                .AddSignalR()
+                .AddAzureSignalR();
+
             services.AddControllers();
             services.AddSwaggerGen();
         }
