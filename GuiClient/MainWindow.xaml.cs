aa374bb732a0dfcd83e51339c6ddc4275a8bbccf
Michael Stenger <michael.stenger@outlook.de>
2021-09-03 08:14:22 +0200

    adding simple gui

--- GuiClient/MainWindow.xaml.cs
+++ GuiClient/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace GuiClient
 {
@@ -23,6 +10,16 @@ namespace GuiClient
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Register(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SendMessage(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
