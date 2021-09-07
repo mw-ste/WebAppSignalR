d4df5827c3ebb1296686e02c3ab69818efb9b267
Michael Stenger <michael.stenger@outlook.de>
2021-09-07 10:36:57 +0200

    implementing view model

--- GuiClient/MainWindowViewModel.cs
+++ GuiClient/MainWindowViewModel.cs
@@ -1,14 +1,123 @@
-﻿using Shared;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
 
 namespace GuiClient
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
-        private readonly ISignalRClient _signalRClient;
+        private readonly SignalRClient _signalRClient;
 
-        public MainWindowViewModel(ISignalRClient signalRClient)
+        private bool _registered;
+        private string _messageLog;
+        private string _infoLog;
+        private string _userName;
+        private string _message;
+        private string _receiver;
+
+        public MainWindowViewModel(SignalRClient signalRClient)
         {
             _signalRClient = signalRClient;
+
+            RegisterCommand = new Command(
+                async _ => await Register(),
+                _ => !Registered && !string.IsNullOrEmpty(UserName));
+
+            SendMessageCommand = new Command(
+                async _ => await SendMessage(),
+                _ => Registered);
         }
+
+        private async Task Register()
+        {
+            await _signalRClient.Register(UserName);
+
+            Registered = true;
+
+            RegisterCommand.NotifyCanExecuteChanged();
+            SendMessageCommand.NotifyCanExecuteChanged();
+        }
+
+        private async Task SendMessage()
+        {
+            if (string.IsNullOrEmpty(Receiver))
+            {
+                await _signalRClient.SendMessageToAllClients(Message);
+            }
+            else
+            {
+                await _signalRClient.SendMessageToClient(Receiver, Message);
+            }
+
+            Receiver = string.Empty;
+            Message = string.Empty;
+        }
+
+        public Command RegisterCommand { get; }
+        public Command SendMessageCommand { get; }
+
+        public bool Registered
+        {
+            get => _registered;
+            set => SetProperty(nameof(Registered), ref _registered, value);
+        }
+
+        public string MessageLog
+        {
+            get => _messageLog;
+            set => SetProperty(nameof(MessageLog), ref _messageLog, value);
+        }
+
+        public string InfoLog
+        {
+            get => _infoLog;
+            set => SetProperty(nameof(InfoLog), ref _infoLog, value);
+        }
+
+        public string UserName
+        {
+            get => _userName;
+            set => SetProperty(nameof(UserName), ref _userName, value);
+        }
+        public string Message
+        {
+            get => _message;
+            set => SetProperty(nameof(Message), ref _message, value);
+        }
+        public string Receiver
+        {
+            get => _receiver;
+            set => SetProperty(nameof(Receiver), ref _receiver, value);
+        }
+
+        private void SetProperty<T>(string propertyName, ref T currentValue, T newValue)
+        {
+            if (EqualityComparer<T>.Default.Equals(currentValue, newValue))
+            {
+                return;
+            }
+
+            currentValue = newValue;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
+
+/*
+ * _logInfo($"Reconnecting. Connection was interrupted because of \"{exception}\"");
+ * _logInfo(
+        $"Hub reconnected with new id {newConnectionId}, " +
+        $"new connection state \"{_hubConnection.State}\"");
+
+    _logInfo(
+        $"Hub connection {_hubConnection.ConnectionId} was closed!\n" +
+        $"Reason: {exception}");
+
+    _logInfo("Message successfully sent");
+
+    _logInfo($"{user} joined the conversation");
+
+    _logInfo($"{user} left the conversation");
+ */
