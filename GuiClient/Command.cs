96498a00c93a98c2dde483af92c58384f522ba69
Michael Stenger <michael.stenger@outlook.de>
2021-09-07 10:13:09 +0200

    adding delegate command with support for async execution

---
+++ GuiClient/Command.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace GuiClient
+{
+    internal class Command : ICommand
+    {
+        private bool _isExecuting;
+        private bool IsExecuting
+        {
+            get => _isExecuting;
+            set
+            {
+                if (value == _isExecuting)
+                {
+                    return;
+                }
+
+                _isExecuting = value;
+                NotifyCanExecuteChanged();
+            }
+        }
+
+        private readonly Func<object, Task> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public Command(Func<object, Task> execute) 
+            : this(execute, _ => true)
+        {
+        }
+
+        public Command(
+            Func<object, Task> execute,
+            Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public async void Execute(object parameter)
+        {
+            if (!CanExecute(parameter))
+            {
+                return;
+            }
+
+            try
+            {
+                IsExecuting = true;
+                await _execute(parameter);
+            }
+            finally
+            {
+                IsExecuting = false;
+            }
+        }
+
+        public bool CanExecute(object parameter) => !IsExecuting && _canExecute(parameter);
+
+        public void NotifyCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
